ae41b9a0905379f3add233434a9fa7734874377f
Stephanie Malek <stephanie.g.malek@gmail.com>
2023-11-10 11:47:37 -0600

    added initial integration of important button for todo list items

--- lib/seed-data.sql
+++ lib/seed-data.sql
@@ -1,11 +1,12 @@
-INSERT INTO todolists (title, username)
-  VALUES ('Work Todos', 'admin'),
-         ('Home Todos', 'admin'),
-         ('Additional Todos', 'admin'),
-         ('social todos', 'admin');
+INSERT INTO todolists (id, title, username)
+  VALUES (1, 'Work Todos', 'admin'),
+         (2, 'Home Todos', 'admin'),
+         (3, 'Additional Todos', 'admin'),
+         (4, 'social todos', 'admin');
 
 -- Note: in the following statement, get the todo list IDs from
--- the todolists table. If the todo list IDs are 1, 2, 3, and 4, then our code looks like this:
+-- the todolists table. If the todo list IDs are 1, 2, 3, and 4, then our code
+-- looks like this:
 INSERT INTO todos (title, done, todolist_id, username)
   VALUES ('Get coffee', TRUE, 1, 'admin'),
          ('Chat with co-workers', TRUE, 1, 'admin'),
